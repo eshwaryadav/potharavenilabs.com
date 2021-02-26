aeafc53a25df524e290db5f6896b2370d4cac2ce
omkareshwar <omkary2@gmail.com>
2021-02-26 15:31:10 +0530

    empoly word changed

--- jqueryFun.js
+++ jqueryFun.js
@@ -2,4 +2,7 @@
 $(function(){
     $(".labs").hide();
     $(".labs").show(2000);
+    // $("#container1").hide();$("#container2").hide()
+    // $("#container1").fadeIn(500)
+    // $("#container2").fadeIn(1000)
 })
