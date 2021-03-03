6da198cbd2364da5dd8bc9f78466b3e75306f759
omkareshwar <omkary2@gmail.com>
2021-03-03 19:51:21 +0530

    google adsense

--- jqueryFun.js
+++ jqueryFun.js
@@ -1,7 +1,11 @@
 // $(document).ready(function(){
 $(function(){
-    $(".labs").hide();
-    $(".labs").show(2000);
+    // $(".labs").hide();
+    // $(".labs").show(2000);
+    //j query chaining
+    $(".labs").hide().show(2000)
+
+
     // $("#container1").hide();$("#container2").hide()
     // $("#container1").fadeIn(500)
     // $("#container2").fadeIn(1000)
